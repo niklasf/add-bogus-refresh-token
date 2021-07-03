e16e58948e01916a7c8f15a6e2bc5c8cd1df812b
Niklas Fiekas <niklas.fiekas@backscattering.de>
2021-07-03 09:53:36 +0200

    ready for prod

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require('express');
 const proxy = require('express-http-proxy');
 const app = express();
 const port = 9700;
-const backend = 'http://localhost:9663';
+const backend = 'http://localhost:9004';
 
 app.get('/oauth', proxy(backend));
 
 app.post('/oauth', proxy(backend, {
   userResDecorator: (_proxyRes, proxyResData, _userReq, _userRes) => {
     const data = JSON.parse(proxyResData.toString('utf8'));
-    if (data.access_token && !data.refresh_token) data.refresh_token = 'invalid_for_bc_' + Math.floor(Math.random() * 10000000);
+    if (data.access_token && !data.refresh_token) {
+      data.refresh_token = 'invalid_for_bc_' + Math.floor(Math.random() * 10000000);
+      console.log(`added ${data.refresh_token}`);
+    }
     return JSON.stringify(data);
   }
 }));
