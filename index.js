bbd518afce5920e3f825fa83abd95831479496d6
Niklas Fiekas <niklas.fiekas@backscattering.de>
2021-07-03 09:45:08 +0200

    add bogus refresh token

--- index.js
+++ index.js
@@ -6,4 +6,12 @@ const backend = 'http://localhost:9663';
 
 app.get('/oauth', proxy(backend));
 
+app.post('/oauth', proxy(backend, {
+  userResDecorator: (_proxyRes, proxyResData, _userReq, _userRes) => {
+    const data = JSON.parse(proxyResData.toString('utf8'));
+    if (data.access_token && !data.refresh_token) data.refresh_token = 'invalid_for_bc_' + Math.floor(Math.random() * 10000000);
+    return JSON.stringify(data);
+  }
+}));
+
 app.listen(port);
